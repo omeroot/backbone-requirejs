71816a41212b10d7e601670f64e38c15ab563fd0
omeroot <demircanomer91@gmail.com>
2015-09-09 10:38:40 +0300

    in

--- app.js
+++ app.js
@@ -3,39 +3,39 @@ var bodyParser = require('body-parser');
 var app = express();
 
 var db = {
-	"books": [
-		{
-			"title": "backbone",
-			"author": "omer",
-			"releaseDate": "10-10-1991",
-			"keywords": "omer,backbone"
+  "books": [
+    {
+      "title": "backbone",
+      "author": "omer",
+      "releaseDate": "10-10-1991",
+      "keywords": "omer,backbone"
     },
-		{
-			"title": "node pattern",
-			"author": "omer",
-			"releaseDate": "10-2-1991",
-			"keywords": "omer,node,javascript"
+    {
+      "title": "node pattern",
+      "author": "omer",
+      "releaseDate": "10-2-1991",
+      "keywords": "omer,node,javascript"
     }
   ]
 };
 
 app.configure(function () {
-	app.set('view engine', 'jade');
-	app.use(express.static(__dirname + '/public'));
+  app.set('view engine', 'jade');
+  app.use(express.static(__dirname + '/public'));
   app.use(bodyParser());
 });
 
 app.get('/', function (req, res) {
-	res.render('index.jade', {
-		layout: false
-	});
+  res.render('index.jade', {
+    layout: false
+  });
 });
 app.get('/books', function (req, res) {
-	console.log('books send', db.books);
-	res.json(db.books);
+  console.log('books send', db.books);
+  res.json(db.books);
 });
 app.get('/books/:id', function (req, res) {
-	res.json(db.books[id]);
+  res.json(db.books[id]);
 });
 app.post('/books', function (req, res) {
   db.books.push({
